972064f17673fb1c184640f7caed1defdd2db792
czabobo <58294360+czabobo@users.noreply.github.com>
2019-12-01 09:47:52 +0800

    Update BookApplication.java

--- src/main/java/com/baizhi/BookApplication.java
+++ src/main/java/com/baizhi/BookApplication.java
@@ -9,6 +9,8 @@ public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
         System.out.println("111");
+System.out.println("222");
+System.out.println("3");
     }
 
 }
