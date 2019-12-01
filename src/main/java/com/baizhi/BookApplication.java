2d9440d6368e4cdac4479aa7a5ecf4a4c01f2bec
cwb <173517623@qq.com>
2019-12-01 09:49:17 +0800

    dev修改12

--- src/main/java/com/baizhi/BookApplication.java
+++ src/main/java/com/baizhi/BookApplication.java
@@ -9,8 +9,6 @@ public class BookApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
         System.out.println("111");
-        System.out.println("dev分支");
-        System.out.println("dev分支修改");
     }
 
 }
